0d174abae0414a1d46beac03f396261700853124
Faris Chebib <octaflop@gmail.com>
2011-07-12 18:22:49 -0700

    added README. Prepped for gihub ☺

---
+++ README.md
@@ -0,0 +1,3 @@
+**Borsa**
+
+Borsa is a bag of django. Do not use it for production applications as it is merely the sandbox of a crazy-person.
--- borsa/settings.py
+++ borsa/settings.py
@@ -133,7 +133,7 @@ ADMIN_MEDIA_PREFIX = "/media/"
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash if there is a path component (optional in other cases).
 # Examples: "http://media.lawrence.com", "http://example.com/media/"
-MEDIA_URL = "/site_media/media/"
+MEDIA_URL = "/media/"
 
 # Absolute path to the directory that holds media.
 MEDIA_ROOT = os.path.join(PROJECT_ROOT, "site_media", "media")
